e8d07037b9679dbf2ea4c192576a838035f56b1f
Rishi K. <contact.rishikmr@gmail.com>
2024-08-21 22:09:12 +0530

    fix(TabCompletion): Handle tilde and spaces in paths for the FZF preview command
    Problem:
    - The `ls` command in the FZF preview is falling for the directory paths containing `~` and spaces in them.
    - Tilde expansion isn't happening for the directory paths starting with tilde `~` to the `ls` command in the FZF preview.
    - Directory paths containing spaces in them can break the `{}` subsitution in FZF preview, because the shell would treat the parts separated by spaces as separate arguments. This causes `ls` to fail on paths with spaces.
    
    Solution:
    - Use `bash -c '...' -- {}` in FZF preview.
    - This explicitly treats the selected item (the directory path) as an argument to the `bash -c` command.
    - The `--` tells bash that everything following it should be treated as positional parameters (arguments) rather than part of the script.
    - This means, the selected path is passed as the first argument (`$1`) to the `bash -c` script.
    - Now the directory path can be safely referred as `$1` inside the script, to the `ls` command in the FZF preview command.

--- navita.sh
+++ navita.sh
@@ -291,7 +291,7 @@ __navita::completions() {
 		esac
 	else
 		COMPREPLY=( $( compgen -d -- "${COMP_WORDS[COMP_CWORD]}" | \
-			fzf --prompt="navita> " --tiebreak=begin,index --scheme=history --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up --preview-window=down --border=bold --preview="ls -lashFd --color=always {} && echo && ls -CFaA --color=always {}" ) )
+			fzf --prompt="navita> " --tiebreak=begin,index --scheme=history --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up --preview-window=down --border=bold --preview="bash -c 'ls -lashFd --color=always \"\${1/#~/${HOME}}\" && echo && ls -CFaA --color=always \"\${1/#~/${HOME}}\"' -- {}" ) )
 	fi
 }
 
