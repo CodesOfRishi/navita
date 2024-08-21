572974b3fb2595439fd35289341e1c2e02c5dada
Rishi K. <contact.rishikmr@gmail.com>
2024-08-21 20:00:01 +0530

    feat(TabCompletion): introduce tab completion for Navita options and directories in PWD
    - If `-` is the first character of the first argument, tab press will bring a fzf list of Navita options and attempt to exact match with the argument under cursor.
    - In all other cases, tab press will bring a fzf list of directories from PWD and attempt to exact match with the argument under cursor.
    - String matches in FZF will be closer to the beginning of the line.

--- navita.sh
+++ navita.sh
@@ -271,3 +271,30 @@ __navita__() {
 
 # update the history with the current working directory when opening a new shell
 __navita::UpdatePathHistory
+
+# ── Feature: TabCompletion ────────────────────────────────────────────{{{
+__navita::completions() {
+	if [[ "${COMP_CWORD}" -eq 1 ]] && [[ "${COMP_WORDS[COMP_CWORD]}" =~ ^- ]]; then
+		local navita_opts && navita_opts="$( printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" "-" "--" "-H" "--history" "-c" "--clean" "-s" "--sub-search" "-S" "--super-search" "-v" "--version" | \
+			fzf --prompt="navita> " --tiebreak=begin,index --scheme=history --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up )"
+
+		case "$?" in
+			0) COMPREPLY=( "${navita_opts}" );;
+			*) 
+				local dir_select && dir_select="$( find -L ./ -maxdepth 1 -mindepth 1 -type d | fzf --prompt="navita> " --tiebreak=begin,index --scheme=history --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up --preview-window=down --border=bold --preview="ls -lashFd --color=always {} && echo && ls -CFaA --color=always {}" )"
+
+				case "$?" in
+					0) COMPREPLY=( "${dir_select}" );;
+					*) return 0;;
+				esac
+				;;
+		esac
+	else
+		COMPREPLY=( $( compgen -d -- "${COMP_WORDS[COMP_CWORD]}" | \
+			fzf --prompt="navita> " --tiebreak=begin,index --scheme=history --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up --preview-window=down --border=bold --preview="ls -lashFd --color=always {} && echo && ls -CFaA --color=always {}" ) )
+	fi
+}
+
+complete -F __navita::completions "${NAVITA_COMMAND}"
+# }}}
+
