9b56b946c8fa4ff9edc531a61242cf5467947b8a
Rishi K. <contact.rishikmr@gmail.com>
2024-09-14 12:52:54 +0530

    fix(Bash:TabCompletion): prevent terminal corruption on FZF closure when completing directories
    - Added `bind '"\e[0n": redraw-current-line'` and `printf '\e[5n'` to fix terminal visual corruption and misalignment when FZF closes with height less than 100%, ensuring a stable command line interface.
    - The `redraw-current-line` function in Readline causes the current command line in the terminal to be redrawn.
    - `\e[5n` is part of the ANSI Device Status Report (DSR). It requests a report of whether the terminal is busy or not. If the terminal is ready (not busy), it responds with `\e[0n`.
    - `\e[0n` is another DSR sequence and it's being bound to `redraw-current-line`. This means that whenever the terminal outputs the `\e[0n` escape sequence, Bash will interpret it as a signal to redraw the current command line.

--- navita.sh
+++ navita.sh
@@ -559,15 +559,17 @@ __navita__() {
 
 # ── Feature: TabCompletion ────────────────────────────────────────────{{{
 if [[ -n "${BASH_VERSION}" ]]; then
+	# To redraw line after fzf closes (printf '\e[5n') 
+	# This is useful when the terminal is altered by FZF, and the command line gets visually corrupted or misaligned
+	bind '"\e[0n": redraw-current-line' 2> /dev/null
 	__navita::completions() {
 		__navita::completions::CompleteDirectory() {
 			local dir_select && dir_select="$( compgen -d -- "${COMP_WORDS[COMP_CWORD]}" | \
 				"${navita_depends["fzf"]}" --prompt='❯ ' --info='inline: ❮ ' --info-command='echo -e "\x1b[33;1m${FZF_INFO%%/*}\x1b[m/${FZF_INFO##*/} Directory completion « Navita"' --height "40%" --tiebreak=begin,index --select-1 --exit-0 --exact --layout=reverse --query="${COMP_WORDS[COMP_CWORD]}" --bind=tab:down,btab:up --preview-window=down --border=bold --preview="bash -c '${navita_depends["ls"]} -lashFd --color=always -- \"\${1/#~/${HOME}}\" && echo && ${navita_depends["ls"]} -CFaA --color=always -- \"\${1/#~/${HOME}}\"' -- {}" )"
 
-			case "$?" in
-				0) COMPREPLY=( "${dir_select}/" );;
-				*) return 0;;
-			esac
+			[[ "$?" -eq 0 ]] && dir_select="${dir_select}/"
+			COMPREPLY=( "${dir_select}" )
+			printf '\e[5n'
 		}
 	
 		if (( COMP_CWORD == 1 )) && [[ "${COMP_WORDS[COMP_CWORD]}" =~ ^- ]]; then
