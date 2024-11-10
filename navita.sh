e8bdedcb3c4ac1ca0276cb71c3a9d27d1f55a005
Rishi K. <contact.rishikmr@gmail.com>
2024-11-10 10:36:38 +0530

    fix: return correct exit code when no directory match is found
    - Return correct exit code when no directory match is found during Parent-directory, Sub-directory, Highest-ranked directory and history navigation.
    - Change version from 'v2.3.7' to 'v2.3.7+dev'.

--- navita.sh
+++ navita.sh
@@ -60,7 +60,7 @@ export NAVITA_HISTORYFILE="${NAVITA_DATA_DIR}/navita-history"
 export NAVITA_FOLLOW_ACTUAL_PATH="${NAVITA_FOLLOW_ACTUAL_PATH:-n}"
 export NAVITA_COMMAND="${NAVITA_COMMAND:-cd}"
 export NAVITA_HISTORY_LIMIT="${NAVITA_HISTORY_LIMIT:-100}"
-export NAVITA_VERSION="v2.3.7"
+export NAVITA_VERSION="v2.3.7+dev"
 export NAVITA_CONFIG_DIR="${NAVITA_CONFIG_DIR:-${XDG_CONFIG_HOME:-$HOME/.config}/navita}"
 export NAVITA_IGNOREFILE="${NAVITA_CONFIG_DIR}/navita-ignore"
 export NAVITA_RELATIVE_PARENT_PATH="${NAVITA_RELATIVE_PARENT_PATH:-y}"
@@ -586,7 +586,7 @@ __navita::NavigateHistory() {
 			builtin cd "${__the_builtin_cd_option[@]}" -- "${path_returned}" || return $?
 			(__navita::UpdatePathHistory &)
 			;;
-		1) printf "navita: None matched!\n" >&2; return $?;;
+		1) printf "navita: None matched!\n" >&2; return 1;;
 		*) return $?;;
 	esac
 }
@@ -612,7 +612,7 @@ __navita::NavigateChildDirs() {
 			builtin cd "${__the_builtin_cd_option[@]}" -- "${path_returned}" || return $?
 			(__navita::UpdatePathHistory &)
 			;;
-		1) printf "navita: None matched!\n" >&2; return $?;;
+		1) printf "navita: None matched!\n" >&2; return 1;;
 		*) return $?;;
 	esac
 }
@@ -658,7 +658,7 @@ __navita::NavigateParentDirs() {
 			builtin cd "${__the_builtin_cd_option[@]}" -- "${path_returned}" || return $?
 			(__navita::UpdatePathHistory &)
 			;;
-		1) printf "navita: None matched!\n" >&2; return $?;;
+		1) printf "navita: None matched!\n" >&2; return 1;;
 		*) return $?;;
 	esac
 }
@@ -739,7 +739,7 @@ __navita::CDGeneral() {
 			builtin cd "${__the_builtin_cd_option[@]}" -- "${path_returned}" || return $?
 			(__navita::UpdatePathHistory &)
 			;;
-		1) printf "navita: None matched!\n" >&2; return $?;;
+		1) printf "navita: None matched!\n" >&2; return 1;;
 		*) return "$?";;
 	esac
 }
